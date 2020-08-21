crdroidandroid/android_lineage-sdk

crdroid: Do better for version pref
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

sdk/src/java/org/lineageos/internal/preference/deviceinfo/LineageVersionTextView.java
@@ -18,6 +18,7 @@ package org.lineageos.internal.preference.deviceinfo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.util.AttributeSet;
@@ -31,10 +32,6 @@ public class LineageVersionTextView extends TextView implements View.OnClickList
     private static final String TAG = "LineageVersionTextView";
 
     private static final String KEY_LINEAGE_VERSION_PROP = "ro.modversion";
-
-    private static final String PLATLOGO_PACKAGE_NAME = "org.lineageos.lineageparts";
-    private static final String PLATLOGO_ACTIVITY_CLASS =
-            PLATLOGO_PACKAGE_NAME + ".logo.PlatLogoActivity";
 
     private long[] mHits = new long[3];
 
@@ -50,13 +47,13 @@ public class LineageVersionTextView extends TextView implements View.OnClickList
         System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
         mHits[mHits.length - 1] = SystemClock.uptimeMillis();
         if (mHits[0] >= (SystemClock.uptimeMillis() - 500)) {
-            launchLogoActivity();
+            launchUrl("https://www.crdroid.net");
         }
     }
 
-    private void launchLogoActivity() {
-        final Intent intent = new Intent(Intent.ACTION_MAIN)
-                .setClassName(PLATLOGO_PACKAGE_NAME, PLATLOGO_ACTIVITY_CLASS);
+    private void launchUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uriUrl);
         try {
             getContext().startActivity(intent);
         } catch (Exception e) {

Fluid-Wip/lineage-sdk

sdk/src/java/org/lineageos/internal/preference/deviceinfo/LineageVersionTextView.java
@@ -18,6 +18,7 @@ package org.lineageos.internal.preference.deviceinfo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.util.AttributeSet;
@@ -31,10 +32,6 @@ public class LineageVersionTextView extends TextView implements View.OnClickList
     private static final String TAG = "LineageVersionTextView";
 
     private static final String KEY_LINEAGE_VERSION_PROP = "ro.lineage.version";
-
-    private static final String PLATLOGO_PACKAGE_NAME = "org.lineageos.lineageparts";
-    private static final String PLATLOGO_ACTIVITY_CLASS =
-            PLATLOGO_PACKAGE_NAME + ".logo.PlatLogoActivity";
 
     private long[] mHits = new long[3];
 
@@ -50,13 +47,13 @@ public class LineageVersionTextView extends TextView implements View.OnClickList
         System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
         mHits[mHits.length - 1] = SystemClock.uptimeMillis();
         if (mHits[0] >= (SystemClock.uptimeMillis() - 500)) {
-            launchLogoActivity();
+            launchUrl("https://www.fluidos.me");
         }
     }
 
-    private void launchLogoActivity() {
-        final Intent intent = new Intent(Intent.ACTION_MAIN)
-                .setClassName(PLATLOGO_PACKAGE_NAME, PLATLOGO_ACTIVITY_CLASS);
+    private void launchUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uriUrl);
         try {
             getContext().startActivity(intent);
         } catch (Exception e) {